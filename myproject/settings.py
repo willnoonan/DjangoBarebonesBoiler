05b4ca44e31aac8b79d33bd937e35e938c0de558
williamnoonan <willnoonan@outlook.com>
2021-11-14 18:36:20 -0600

    Added app "main" to house the base HTML templates and serve as a starting point for Django projects. It includes: base.html, which contains Bootstrap links that other HTML templates can extend; navbar.html, which contains the Boostrap navbar component; and index.html, which is the homepage. Setup up the home page view in main/views.py.

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'main',
+
+    # Django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
